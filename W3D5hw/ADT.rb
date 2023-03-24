484f092600fcacd735b097f9d679db379e0037e3
joshua <joshk.5793@gmail.com>
2023-03-24 10:36:31 -0400

    fixes

--- W3D5hw/ADT.rb
+++ W3D5hw/ADT.rb
@@ -7,35 +7,51 @@ class Stack
 
   def push(el)
     # adds an element to the stack
-    @array << el
+    array << el
+    self
   end
 
   def pop
     # removes one element from the stack
-    @array.pop
+    array.pop
   end
 
   def peek
     # returns, but doesn't remove, the top element in the stack
-    @array.last
+    array.last
   end
+
+  private
+  attr_reader :array
 end
 
 
-class Queue
+class MyQueue
     def initialize
+      @inner_array = []
 
     end
 
-    def enqueue(ele)
+    def enqueue(ele) #Add element to back of queue
+      inner_array.unshift(ele)
     end
 
-    def dequeue
+    def dequeue # Remove and return element from front of queue
+      inner_array.pop
     end
 
+    def show 
+      inner_array.dup # dup shows original array, doesn't manipulate original array
+    end
 
+    def empty?
+      inner_array.empty?
+    end
     def peek
     end
+
+    private
+    attr_reader :inner_array
 
 end
 
@@ -54,11 +70,11 @@ class Map
     def delete(key)
     end
 
-    def show
+    def show #show the entire queue but dont return it!
 
     end
 
-    def set
+    def set 
     end
 
 
