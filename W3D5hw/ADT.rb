7600f0ff06412000f812671753536159ba23c1ce
joshua <joshk.5793@gmail.com>
2023-03-23 22:22:22 -0400

    first commit

---
+++ W3D5hw/ADT.rb
@@ -0,0 +1,39 @@
+class Stack
+  def initialize
+    # create ivar to store stack here!
+
+  end
+
+  def push(el)
+    # adds an element to the stack
+    self.push(el)
+  end
+
+  def pop
+    # removes one element from the stack
+    self.pop
+  end
+
+  def peek
+    # returns, but doesn't remove, the top element in the stack
+    self.peek
+  end
+end
+
+
+class Queue
+    def initialize
+
+    end
+
+    def enqueue(ele)
+    end
+
+    def dequeue
+    end
+
+
+    def peek
+    end
+
+end
